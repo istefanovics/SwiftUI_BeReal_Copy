584b9e436205b7b9363670b65cae5a967413db98
Istvan Stefanovics <istvan.stefanovics@finshape.com>
2023-09-01 09:18:29 +0200

    Initial commit

---
+++ BeRealCourse/BeRealCourse/BeRealCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealCourseApp.swift
+//  BeRealCourse
+//
+//  Created by István Stefánovics on 2023. 08. 31..
+//
+
+import SwiftUI
+
+@main
+struct BeRealCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
